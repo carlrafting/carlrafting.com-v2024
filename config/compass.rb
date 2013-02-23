052a5d1305773021b9cc23afdd8be6a000c23a57
Carl Räfting <carl.rafting@gmail.com>
2013-02-23 14:42:54 +0100

    Imported posts from Textpattern. Configured compass config file.

--- config/compass.rb
+++ config/compass.rb
@@ -1,5 +1,7 @@
 # Require any additional compass plugins here.
 
+# run ´$ compass compile --force -e production´ for compiling production stylesheets
+
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
@@ -9,17 +11,22 @@ javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :expanded
+output_style = (environment == :development) ? :expanded : :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-
+if environment == :production
+  line_comments = false
+end
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+if environment == :development
+  sass_options = { :debug_info => true }
+end
