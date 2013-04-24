6b60e37d0e86632e5e10e5b5e7a96902a6afda10
Carl Räfting <carl.rafting@gmail.com>
2013-04-25 00:03:56 +0200

    Ship it.

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      options: {
+        compass: 'compass.rb'
+      },
+      prod: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'css/global.min.css': 'scss/global.scss'
+        }
+      },
+      dev: {
+        options: {
+          debugInfo: true,
+          lineNumbers: true
+        },
+        files: {
+          'css/global.css': 'scss/global.scss'
+        }
+      }
+    },
+    watch: {
+      sass: {
+        files: ['scss/*.scss', 'scss/**/*.scss'],
+        tasks: 'sass'
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  
+  // Default task.
+  grunt.registerTask('default', ['sass']);
+
+};
